35c761bd983a0718a3350a8b0f55ff150abd2489
Blas-U <cheezypizzagamer@gmail.com>
2023-02-08 14:34:56 -0600

    update

--- webpage/test.php
+++ webpage/test.php
@@ -1,10 +1,15 @@
 <?php
 
     echo"Hi <b>Brooke</b>, why are you late ";
-    echo"and you are too coen!!!";
+    echo"and you are too coen!!!<br>";
 
     print_r($_GET);
-    echo "Hello" . $_GET["name"];
+    echo "Hello " . $_GET["name"];
+
+    foreach ($_GET as $id){
+        echo $id  . "==" . $_GET[$id] . "<br";
+    }
+
     $f = fopen("test.json", "a");
     fwrite($f, "This is a file.");
     fclose($f);
